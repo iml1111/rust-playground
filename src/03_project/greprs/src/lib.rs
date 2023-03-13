86bcc1fa71b1e820ce344ae05e4e8caad9df4062
IML <shin10256@gmail.com>
2023-03-13 21:55:00 +0900

    i/o 프로젝트 완성

--- src/03_project/greprs/src/lib.rs
+++ src/03_project/greprs/src/lib.rs
@@ -1,11 +1,12 @@
 use std::error::Error;
 use std::fs::File;
 use std::io::prelude::*;
+use std::env;
 
-// 여기만 pub만 다 따라오나?
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
@@ -17,7 +18,10 @@ impl Config {
 
         let query = args[1].clone();
         let filename = args[2].clone();
-        Ok(Config { query, filename })
+        // is_err: 해당 Result의 결과가 Error인지 true/false 반환
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config { query, filename, case_sensitive })
     }
 }
 
@@ -27,9 +31,41 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 	let mut contents = String::new();
 	f.read_to_string(&mut contents)?;
 
-    println!("Text: \n{}", contents);
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
+        println!("{}", line);
+    }
 
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 // https://rinthel.github.io/rust-lang-book-ko/ch12-04-testing-the-librarys-functionality.html 
@@ -37,9 +73,31 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 mod test {
     use super::*;
 
-    *[test]
+    #[test]
     fn one_result() {
         let query = "duct";
-        let contents = ""
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, Productive.
+Trust me.";
+        
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
--- src/03_project/greprs/src/main.rs
+++ src/03_project/greprs/src/main.rs
@@ -9,15 +9,12 @@ use greprs::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Config problem: {}", err);
+        eprintln!("Config problem: {}", err);
         process::exit(1);
     });
-
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
     
     if let Err(e) = greprs::run(config) {
-        println!("Applicaiton error: {}", e);
+        eprintln!("Applicaiton error: {}", e);
         process::exit(1);
     }
 }
