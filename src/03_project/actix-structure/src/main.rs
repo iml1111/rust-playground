be39264b60dfaa690550bf61fb9c7fa6d668bdbe
IML <29897277+iml1111@users.noreply.github.com>
2023-04-08 02:11:55 +0900

    Dockerfile 초기 모듈 작성

--- src/03_project/actix-structure/src/main.rs
+++ src/03_project/actix-structure/src/main.rs
@@ -36,7 +36,7 @@ async fn main() -> std::io::Result<()> {
     })
     .keep_alive(KeepAlive::Os) 
     .workers(4)
-    .bind(("127.0.0.1", 5000))?
+    .bind(("127.0.0.1", 8080))?
     .run().await
 }
 
